55f31d6b659ef80932d5cda78400033ef93b25fd
Badimu.Kazadi <badimujosh@gmail.com>
2017-10-05 16:00:07 +0200

    Upgraded the design and one form. The rest will be modified has the time goes

---
+++ Online Book Front-End/Main.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="C:\Users\Badimu.Kazadi\Documents\Visual Studio 2015\Projects\Online Book Store Front-End\Online Book Front-End\Online Book Front-End\scripts/angular.js" />
+
+
+var myApp = angular.module("myApp", ['ngRoute', 'Services']);
+
+myApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+
+
+            when('/Home', {
+                templateUrl: 'View/Home.html',
+                controller: 'HomeController'
+            }).
+            when('/Register', {
+                templateUrl: 'View/Register.html',
+                controller: 'SignUpController'
+            }).
+            when('/LogIn', {
+                templateUrl: 'View/Login.html',
+                controller: 'LogInController'
+            }).
+
+            when('/Profile',
+            {
+                templateUrl: 'View/Profile.html',
+                controller: 'UpdateProfileController'
+            }).
+
+            when('/Book',
+            {
+                templateUrl: 'View/Property.html',
+                controller: 'BookController'
+            }). /*
+            when('/Upload',
+            {
+                templateUrl: 'View/UploadImage.html',
+                controller: 'ImageController'
+            }).
+            */
+            otherwise({ redirectTo: '/Home' });
+    }
+
+
+]);
---
+++ Online Book Front-End/Services.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="C:\Users\Badimu.Kazadi\Documents\Visual Studio 2015\Projects\Online Book Store Front-End\Online Book Front-End\Online Book Front-End\scripts/angular.js" />
+
+
+var Services = angular.module('Services', []);
+
+Services.factory('userApi', function ($http, $q)
+
+          {
+
+                var apiURL = "";
+                var userApi = {};
+
+
+
+
+
+
+          }
+
+
+
+
+
+
+
+)
